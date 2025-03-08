fdb36d5407b0a4a621f242bbcd09f272f602f733
Nikita <nikita.200210@mail.ru>
2025-03-08 14:04:07 +0300

    add logger

--- presenter/presenter.py
+++ presenter/presenter.py
@@ -1,3 +1,14 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='./logger/app.log',
+    filemode='a'
+)
+
+logging.info("Логирование запущено")
+
 from model.model_translate import ModelTranslate
 from view.view_translate import ViewTranslate
 from config.constants import LANGUAGES_SOURCE, LANGUAGES_TARGET
@@ -15,10 +26,13 @@ class PresenterTranslate:
         text = self.view.current_text()
         current_source_lang = LANGUAGES_SOURCE[self.view.current_source_lang()]
         current_target_lang = LANGUAGES_TARGET[self.view.current_target_lang()]
-        translated_text = self.model.translate_text(
+        translated_object = self.model.translate_text(
             text, current_target_lang, current_source_lang
         )
-        if translated_text["error"] is not None:
+        if translated_object["error"] is not None:
             self.view.message(Condition.ERROR, "Проверьте соединение с интернетом")
+            logging.info('Приложение не отвечает. Нет интернета.')
         else:
-            self.view.write_translate_in_browser(translated_text["translate"].text)
+            translated_text = translated_object["translate"].text
+            logging.info(f'Текст: {text} ({translated_object["source_lang"]}); Перевод: {translated_text} ({current_target_lang})')
+            self.view.write_translate_in_browser(translated_text)
